0b6fc0c09aed85b8d4fc085971dce7e0729e78bb
Khaled-Oudenani <khaoud1234@gmail.com>
2026-03-27 01:04:49 +0100

    edite cookies error

--- lib/supabase/server.ts
+++ lib/supabase/server.ts
@@ -1,5 +1,32 @@
+// import { createServerClient } from "@supabase/ssr";
+// import { cookies } from "next/headers";
+
+// export async function createClient() {
+//   const cookieStore = await cookies();
+
+//   return createServerClient(
+//     process.env.NEXT_PUBLIC_SUPABASE_URL!,
+//     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+//     {
+//       cookies: {
+//         getAll() {
+//           return cookieStore.getAll();
+//         },
+//         setAll(cookiesToSet) {
+//           try {
+//             cookiesToSet.forEach(({ name, value, options }) =>
+//               cookieStore.set(name, value, options)
+//             );
+//           } catch {}
+//         },
+//       },
+//     }
+//   );
+// }
+
 import { createServerClient } from "@supabase/ssr";
 import { cookies } from "next/headers";
+import type { CookieMethodsServer } from "@supabase/ssr";
 
 export async function createClient() {
   const cookieStore = await cookies();
@@ -12,14 +39,14 @@ export async function createClient() {
         getAll() {
           return cookieStore.getAll();
         },
-        setAll(cookiesToSet) {
+        setAll(cookiesToSet: any[]) {
           try {
             cookiesToSet.forEach(({ name, value, options }) =>
-              cookieStore.set(name, value, options)
+              cookieStore.set(name, value, options),
             );
           } catch {}
         },
       },
-    }
+    },
   );
 }
